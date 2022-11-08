461e8ac4ea172e13189594f0ce8d350432b84225
Fanessa <fanessah@gmail.com>
2022-11-08 15:17:57 -0500

    played with props, state, styling

---
+++ components/Counter.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { Button, Text, StyleSheet } from 'react-native'
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+    return (
+        <>
+            <Text style={styles.medText}> Counter: {count}</Text>
+            <Button
+                title={"Count + 1"}
+                onPress={() => setCount(count + 1)}
+                color="#227266"
+                accessibilityLabel='Count+1' />
+
+        </>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    medText: {
+        fontSize: 24,
+        margin: 16,
+
+    }
+})
+
+
